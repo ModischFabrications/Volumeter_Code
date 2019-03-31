b1ce4de2405c635e1b9880f332e8f01530ceda8b
MagicManFoli <magicmanfoli@gmail.com>
2019-03-31 21:16:55 +0200

    add debouncing and reading

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,17 @@
 Credits: Modisch Fabrications
 
 TODO: 
-- move into classes
-- smooth input with moving average
 
 \*----------------------------*/
 
 // --- statics, constants & defines
 
-#define PIN_LEDS 3
-#define PIN_BTN 2
-#define PIN_MIC 1
+#define PIN_LEDS PB1
+// PIN 7 is the only hardware interrupt on ATTiny85 (INT0)
+// attachInterrupt(0,wakeUpFunction, LOW);
+// (ISR are overkill in this case and are ignored)
+#define PIN_BTN PB2
+#define PIN_MIC PB4
 
 #define N_LEDS 14
 
@@ -29,14 +30,43 @@ TODO:
 
 #define N_READINGS 30
 
-// --- functions
-
-
-
-// --------------
+#define T_DEBOUNCE_MS 50
 
 UV_Meter <PIN_LEDS, N_LEDS> uv_meter(DELAY_TO_SAVE);
 Smoothed_Reader <uint8_t, N_READINGS> reader(PIN_MIC);
+
+// --- functions
+
+
+void check_button_debounced(){
+  static unsigned long last_debounce = 0;
+  static bool last_state = HIGH;
+
+  unsigned long now = millis();
+
+  bool button_state = digitalRead(PIN_BTN);
+
+  // recent change means it's still bouncing or somebody clicked
+  if (button_state != last_state){
+    last_debounce = now;
+  }
+
+  if ( (now - last_debounce) > T_DEBOUNCE_MS) {
+    // value is old enough to be stable
+ 
+    // check if a real change has occured
+    if (button_state != last_state){
+      last_state = button_state;
+
+      // this implementation only cares for positive edges
+      if (button_state == LOW){
+        uv_meter.next_mode();
+      }
+    }   
+  }
+}
+
+// --------------
 
 void setup()
 {
@@ -52,10 +82,10 @@ void setup()
 
 void loop()
 {
-  // TODO check button
-  //uv_meter.next_mode();
+  // TODO: this is ugly and could be better
+  // calls uv_meter.next_mode internally
+  check_button_debounced();
 
-  // TODO move to input smoothing
   uv_meter.set_input_level(reader.get_rolling_avg());
 
   delay(1); // ADC minimum
