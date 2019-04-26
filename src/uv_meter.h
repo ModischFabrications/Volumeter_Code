daf4e4d3b02fed02ba1cb4f037af0c84c5875c64
MagicManFoli <magicmanfoli@gmail.com>
2019-04-26 23:46:33 +0200

    fix grammar, add software power limit

--- src/uv_meter.h
+++ src/uv_meter.h
@@ -130,6 +130,8 @@ class UV_Meter
         pinMode(PIN_LEDS, OUTPUT);
 
         FastLED.addLeds<WS2812B, PIN_LEDS, GRB>(this->leds, N_LEDS);
+          // set powerlimit to 5v, 500mA (Fuse size)
+        FastLED.setMaxPowerInVoltsAndMilliamps(5,500); 
     }
 
     /**
